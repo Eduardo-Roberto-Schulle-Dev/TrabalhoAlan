a5cbe68a25d8f25f3704192dbb9c6e55a502455c
Eduardo_Schulle <eduardo.r.schulle@gmail.com>
2023-11-26 15:00:41 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            
+            ViewBag.Tarefas = new List<Pizza>
+            {
+                new Pizza { Nome = "Preparar", Descricao = "A massa", IsSelected = false },
+                new Pizza { Nome = "Colocar", Descricao = "O Recheio", IsSelected = false },
+                new Pizza { Nome = "Assar ", Descricao = "A Pizza", IsSelected = false }
+            };
+
+            return View(ViewBag);
+        }
+    }
+}
---
+++ WebApplication1/Models/Pizza.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models
+{
+    public class Pizza
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public bool IsSelected { get; set; }
+        
+    }
+}
